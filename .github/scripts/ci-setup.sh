d19e47f5e2e20454c0a9559bdbb4e4b0f2c4a412
Kunshan Wang <wks1986@gmail.com>
2025-03-31 11:19:25 +0800

    Do we really need Python2?

--- .github/scripts/ci-setup.sh
+++ .github/scripts/ci-setup.sh
@@ -2,5 +2,5 @@ set -xe
 
 . $(dirname "$0")/common.sh
 
-sudo apt-get install -y build-essential curl git python2 python3 pkg-config sudo vim clang
+sudo apt-get install -y build-essential curl git python3 pkg-config sudo vim clang
 sudo apt-get clean

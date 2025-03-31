c6e0eeba6742b76601d3fc4492f83c423d31b02a
Kunshan Wang <wks1986@gmail.com>
2025-03-31 11:01:25 +0800

    rustup --component should be comma-separated

--- .github/scripts/build-mmtk.sh
+++ .github/scripts/build-mmtk.sh
@@ -3,7 +3,7 @@ set -xe
 . $(dirname "$0")/common.sh
 
 # simply build mmtk-v8 with nogc
-rustup toolchain install $RUSTUP_TOOLCHAIN --target x86_64-unknown-linux-gnu --component clippy rustfmt
+rustup toolchain install $RUSTUP_TOOLCHAIN --target x86_64-unknown-linux-gnu --component clippy,rustfmt
 rustup override set $RUSTUP_TOOLCHAIN
 
 cd $THE_ROOT/mmtk && cargo build --features nogc
